3733cc5f4ffdbd65614016d16faf4b3c1d161a1c
Yonghe Xia <xiayh17@gmail.com>
2022-11-12 19:47:39 +0800

    Data source check.

--- R/geoChina.R
+++ R/geoChina.R
@@ -25,6 +25,9 @@ geoChina <- function(gse='GSE2546',mirror='tencent',destdir=getwd()){
   if(!gse %in% series.accession){
     stop('Your GSE may not be expression by array, or even not a GSE')
   }
+  if (is.null('http://49.235.27.111')) {
+    message("Data source broken.")
+  }
   down=ifelse(as.numeric(gsub('GSE','',gse))<1000,
               paste0('/GEOmirror/GSEnnn/',gse,
                      '_eSet.Rdata'),
@@ -53,4 +56,38 @@ geoChina <- function(gse='GSE2546',mirror='tencent',destdir=getwd()){
                ', destdir=".", AnnotGPL = F, getGPL = F)'
                ))
   return(gset)
+}
+
+## https://community.rstudio.com/t/internet-resources-should-fail-gracefully/49199/11
+gracefully_fail <- function(remote_file) {
+  try_GET <- function(x, ...) {
+    tryCatch(
+      httr::GET(url = x, httr::timeout(1), ...),
+      error = function(e) conditionMessage(e),
+      warning = function(w) conditionMessage(w)
+    )
+  }
+  is_response <- function(x) {
+    class(x) == "response"
+  }
+  
+  # First check internet connection
+  if (!curl::has_internet()) {
+    message("No internet connection.")
+    return(invisible(NULL))
+  }
+  # Then try for timeout problems
+  resp <- try_GET(remote_file)
+  if (!is_response(resp)) {
+    message(resp)
+    return(invisible(NULL))
+  }
+  # Then stop if status > 400
+  if (httr::http_error(resp)) { 
+    httr::message_for_status(resp)
+    return(invisible(NULL))
+  }
+  
+  # If you are using rvest as I do you can easily read_html in the response
+  xml2::read_html(resp)
 }
